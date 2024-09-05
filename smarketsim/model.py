c9b9b7b70711db64bb27e91757935016e75e07b4
Adam Lynch <adamclynch03@gmail.com>
2024-09-05 19:59:54 -0400

    feat: changes subset when fitted

--- smarketsim/model.py
+++ smarketsim/model.py
@@ -1,16 +1,37 @@
 from smarketsim import metalog
 import numpy
 from numpy.random import multivariate_normal
+import pandas
+import datetime
 
-def _assign_stock_keys(changes):
-    return
+
+def _get_change_subset(changes, date, look_back):
+    changes.index = pandas.to_datetime(changes.index)
+    date_min = pandas.Timestamp(changes.index.min())
+    changes = changes[changes.index <= date]
+    start_date = date
+    found = 0
+    while found < look_back and start_date >= date_min:
+        if start_date in changes.index:
+            found += 1
+        start_date -= datetime.timedelta(1)
+    changes = changes[changes.index > start_date]
+    return changes
+
 
 class Model:
     def __init__(self):
         return
-    
+
     def fit(self, changes, date, look_back, mlog_dim):
-        self.stock_keys = _assign_stock_keys(changes)
-        self
-        cov = numpy.cov()
-        return
+        changes = _get_change_subset(changes, date, look_back)
+        if len(changes) == look_back:
+            for ticker in changes.columns:
+                series = changes[ticker]
+                numna = series.isna().sum()
+                print(series.head())
+                print(series.isna().sum())
+            a = 0
+            return True
+        else:
+            return False
