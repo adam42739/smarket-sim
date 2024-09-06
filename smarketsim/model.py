45011d09cb7555beef7704ae01d3db934cb92a92
Adam Lynch <adamclynch03@gmail.com>
2024-09-05 20:26:03 -0400

    feat: model fit

--- smarketsim/model.py
+++ smarketsim/model.py
@@ -23,15 +23,32 @@ class Model:
     def __init__(self):
         return
 
+    def _fit_model(self):
+        self.metadata = {}
+        stacker = []
+        i = 0
+        for ticker in self.changes.columns:
+            series = self.changes[ticker]
+            numna = series.isna().sum()
+            series = series.fillna(0)
+            stacker.append(series)
+            mlog = metalog.Metalog(self.mlog_dim)
+            mlog.fit(series.values)
+            self.metadata[ticker] = {"index": i, "num_na": numna, "mlog": mlog}
+        X = numpy.stack(stacker, axis=0)
+        self.mu = numpy.mean(X, axis=1)
+        self.cov = numpy.corrcoef(X)
+
     def fit(self, changes, date, look_back, mlog_dim):
-        changes = _get_change_subset(changes, date, look_back)
-        if len(changes) == look_back:
-            for ticker in changes.columns:
-                series = changes[ticker]
-                numna = series.isna().sum()
-                print(series.head())
-                print(series.isna().sum())
-            a = 0
+        self.changes = _get_change_subset(changes, date, look_back)
+        self.date = date
+        self.mlog_dim = mlog_dim
+        if len(self.changes) == look_back:
+            self._fit_model()
             return True
         else:
             return False
+        
+    def sample(self):
+        samp = multivariate_normal(self.mu, self.cov)
+        return
