b8a27095a6edd934c68ad6209727abc1f8c5f4e0
Adam Lynch <adamclynch03@gmail.com>
2024-09-05 21:43:22 -0400

    feat: simulation fitting

--- smarketsim/model.py
+++ smarketsim/model.py
@@ -1,22 +1,6 @@
 from smarketsim import metalog
 import numpy
-import pandas
-import datetime
 import scipy.stats
-
-
-def _get_change_subset(changes, date, look_back):
-    changes.index = pandas.to_datetime(changes.index)
-    date_min = pandas.Timestamp(changes.index.min())
-    changes = changes[changes.index <= date]
-    start_date = date
-    found = 0
-    while found < look_back and start_date >= date_min:
-        if start_date in changes.index:
-            found += 1
-        start_date -= datetime.timedelta(1)
-    changes = changes[changes.index > start_date]
-    return changes
 
 
 class Model:
@@ -29,29 +13,22 @@ class Model:
         i = 0
         for ticker in self.changes.columns:
             series = self.changes[ticker]
-            numna = series.isna().sum()
-            series = series.fillna(0)
             stacker.append(series)
             mlog = metalog.Metalog(self.mlog_dim)
-            mlog.fit(series.values)
-            self.metadata[i] = {"ticker": ticker, "num_na": numna, "mlog": mlog}
+            mlog.fit(series.values, self.lr_avg)
+            self.metadata[i] = {"ticker": ticker, "mlog": mlog}
             i += 1
         X = numpy.stack(stacker, axis=0)
-        self.mu = numpy.mean(X, axis=1)
         self.cov = numpy.corrcoef(X)
 
-    def fit(self, changes, date, look_back, mlog_dim):
-        self.changes = _get_change_subset(changes, date, look_back)
-        self.date = date
+    def fit(self, changes, mlog_dim, lr_avg):
+        self.lr_avg = lr_avg
+        self.changes = changes
         self.mlog_dim = mlog_dim
-        if len(self.changes) == look_back:
-            self._fit_model()
-            return True
-        else:
-            return False
+        self._fit_model()
 
     def sample(self):
-        y = numpy.random.multivariate_normal(self.mu, self.cov)
+        y = numpy.random.multivariate_normal(numpy.zeros(len(self.metadata)), self.cov)
         y = scipy.stats.norm.cdf(y)
         samp = {}
         for i in range(0, len(y)):
---
+++ smarketsim/simulation.py
@@ -0,0 +1,44 @@
+from smarketsim import model
+from smarketsim import stocks
+import pandas
+import datetime
+
+
+import numpy
+
+
+def _get_change_subset(changes, date, look_back):
+    changes.index = pandas.to_datetime(changes.index)
+    date_min = pandas.Timestamp(changes.index.min())
+    changes = changes[changes.index <= date]
+    start_date = date
+    found = 0
+    while found < look_back and start_date >= date_min:
+        if start_date in changes.index:
+            found += 1
+        start_date -= datetime.timedelta(1)
+    changes = changes[changes.index > start_date]
+    return changes
+
+
+class Simulation:
+    def __init__(self):
+        return
+
+    def _get_changes(self, base, downloads, tickers, date, step, look_back):
+        self.changes = stocks.get_changes(base, tickers, step, date, downloads)
+        self.changes = _get_change_subset(self.changes, date, look_back)
+        if len(self.changes) == look_back:
+            self.numna = {}
+            for ticker in self.changes:
+                numna = self.changes[ticker].isna().sum()
+                self.changes[ticker] = self.changes[ticker].fillna(0)
+                self.numna[ticker] = numna
+            return True
+        else:
+            return False
+
+    def sim(self, base, downloads, tickers, date, step, look_back, lr_rate, mlog_dim):
+        self._get_changes(base, downloads, tickers, date, step, look_back)
+        self.model = model.Model()
+        self.model.fit(self.changes, mlog_dim, lr_rate)
