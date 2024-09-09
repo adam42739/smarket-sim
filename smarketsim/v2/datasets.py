98104bfbd37f21a40c08277f78d398ee3c3436a3
Adam Lynch <adamclynch03@gmail.com>
2024-09-09 19:45:49 -0400

    feat: random sampling from dataset

--- smarketsim/v2/datasets.py
+++ smarketsim/v2/datasets.py
@@ -1,4 +1,6 @@
 from smarketsim.v2 import mfeature
+import pandas
+import random
 
 
 class Dataset:
@@ -10,7 +12,23 @@ class Dataset:
         self.load_from_parq(parq)
 
     def load_from_parq(self, parq):
+        self.parq = parq
         self.tickers = mfeature.get_metadata(parq)
 
     def split(self, date):
         self.split_date = date
+
+    def rng_sample_pop(self, N, stock_packet):
+        size = 0
+        while size < N:
+            index = random.randrange(0, len(self.tickers))
+            ticker = self.tickers[index]
+            df = mfeature.from_parquet(self.parq, ticker)
+            df = df[df.index < self.split_date]
+            df = df.reindex()
+            for i in range(0, stock_packet):
+                index = random.randrange(0, len(df))
+                if size == 0:
+                    self.df = pandas.DataFrame(columns=df.columns)
+                self.df.loc[size] = df.iloc[index]
+                size += 1
