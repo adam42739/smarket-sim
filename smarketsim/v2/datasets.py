ee4bf8a2d1b9a2fae112629683fd90fb819c9a2f
Adam Lynch <adamclynch03@gmail.com>
2024-09-11 20:10:32 -0400

    feat: change align class

--- smarketsim/v2/datasets.py
+++ smarketsim/v2/datasets.py
@@ -1,6 +1,7 @@
 from smarketsim.v2 import mfeature
 import pandas
 import random
+import numpy
 
 
 class Dataset:
@@ -32,3 +33,34 @@ class Dataset:
                     self.df = pandas.DataFrame(columns=mfeat.df.columns)
                 self.df.loc[size] = mfeat.df.iloc[index]
                 size += 1
+
+
+class ChangeAlign:
+    def __init__(self):
+        return
+
+    def add_date(self, date, changes):
+        self.df.loc[date] = changes
+        self.df = self.df.sort_index(ascending=False)
+
+    def create(self, parq, tickers, date, LC):
+        self.df = pandas.DataFrame()
+        for ticker in tickers:
+            mfeat = mfeature.MFeat()
+            mfeat.from_parq(parq, ticker)
+            changes = mfeat.df[[LC]]
+            changes = changes[changes.index <= date]
+            changes = changes.rename({LC: ticker}, axis="columns")
+            if self.df.empty:
+                self.df = changes
+            else:
+                self.df = self.df.merge(changes, left_on="Date", right_on="Date")
+        self.df = self.df.dropna()
+
+    def compute_corr(self, size):
+        partition = self.df.head(size)
+        stacker = []
+        for ticker in partition.columns:
+            stacker.append(partition[ticker])
+        X = numpy.stack(stacker, axis=0)
+        return numpy.corrcoef(X)
