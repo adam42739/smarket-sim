15bd77f4f0b0e9356cde68fa18dd2e939ba1d074
Adam Lynch <adamclynch03@gmail.com>
2024-09-11 20:10:49 -0400

    feat: model

--- smarketsim/v2/model.py
+++ smarketsim/v2/model.py
@@ -4,6 +4,8 @@ from sklearn.neighbors import NearestNeighbors
 from smarketsim.v2 import metalog
 import numpy
 import pickle
+from numpy.random import multivariate_normal
+from scipy.stats import norm
 
 
 DSET_N = 1000
@@ -14,6 +16,8 @@ PCA_KEEP = 2
 KNN_NEIGHBOORS = 50
 
 MLOG_PREC_COUNT = 10
+
+MLOG_DIM = 5
 
 
 class MetaModel:
@@ -42,7 +46,7 @@ class MetaModel:
         for i in range(0, MLOG_PREC_COUNT):
             self.mlog_percs[iter].append(self._perc(i)[iter])
 
-    def _config_mlog(self, y_col, mlog_dim):
+    def _config_mlog(self, y_col):
         self.knn = NearestNeighbors(n_neighbors=KNN_NEIGHBOORS)
         self.knn.fit(self.pca_Xtran)
         self.mlog_percs = {}
@@ -60,7 +64,7 @@ class MetaModel:
                 array = []
                 for index in indexes:
                     array.append(self.dset.df.at[index, y_col])
-                self.mlogs[i][j] = metalog.Metalog(mlog_dim)
+                self.mlogs[i][j] = metalog.Metalog(MLOG_DIM)
                 self.mlogs[i][j].fit(array)
 
     def clean_fitting_data(self):
@@ -69,10 +73,10 @@ class MetaModel:
         del self.knn
         del self.pca_Xtran
 
-    def fit_parq(self, parq, X_cols, y_col, train_before, mlog_dim):
+    def fit_parq(self, parq, X_cols, y_col, train_before):
         self._config_dset(parq, train_before)
         self._perform_PCA(X_cols)
-        self._config_mlog(y_col, mlog_dim)
+        self._config_mlog(y_col)
 
     def _get_perc_index(self, val, iter):
         index = 0
@@ -102,6 +106,50 @@ def read_metamodel(path):
     return mod
 
 
-class CorrModel:
+class Model:
     def __init__(self):
         return
+
+    def fit_parq(
+        self,
+        parq,
+        desc,
+        tickers,
+        date,
+    ):
+        self.indexes = {tickers[i]: i for i in range(0, len(tickers))}
+        self.models = {}
+        for y_col in desc:
+            metamodel = MetaModel()
+            metamodel.fit_parq(parq, desc[y_col]["X_cols"], y_col, date)
+            calgn = datasets.ChangeAlign()
+            calgn.create(parq, tickers, date, desc[y_col]["LC"])
+            self.models[y_col] = {"metamodel": metamodel, "calgn": calgn}
+
+    def predict(self, corr_size, data_series):
+        res = {}
+        for y_col in self.models:
+            corr = self.models[y_col]["calgn"].compute_corr(corr_size)
+            mlogs = {}
+            for ticker in data_series:
+                series = data_series[ticker]
+                mlog = self.models[y_col]["metamodel"].predict(series)
+                mlogs[ticker] = mlog
+            res[y_col] = {"corr": corr, "mlogs": mlogs}
+        return res
+
+    def sample(self, corr_size, data_series):
+        res = self.predict(corr_size, data_series)
+        samp = {}
+        for y_col in res:
+            norm_Z = multivariate_normal(
+                numpy.zeros(len(data_series)), res[y_col]["corr"]
+            )
+            norm_P = norm.cdf(norm_Z)
+            changes = {}
+            for ticker in data_series:
+                mlog = res[y_col]["mlogs"][ticker]
+                index = self.indexes[ticker]
+                changes[ticker] = mlog.quantile(norm_P[index])
+            samp[y_col] = changes
+        return samp
