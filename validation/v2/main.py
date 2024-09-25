cadbeecb32081cacdf9c2bf238e429a33ddd4e11
Adam Lynch <adamclynch03@gmail.com>
2024-09-25 19:15:08 -0400

    feat: v2 parq building

--- validation/v2/main.py
+++ validation/v2/main.py
@@ -1,17 +1,19 @@
-import smarketsim
+import smarketsim.v2 as smarketsim
 import datetime
-import yfscraper
+import yfscraper.v2 as yfscraper
 import json
 
 
 TICKERS = "validation/tickers.json"
 FAILED = "validation/failed.json"
 BASE = "validation/base/"
-SIM_FILES = "validation/v1/sim_files/"
-SIM_PERCS = "validation/v1/sim_percs.json"
+SIM_FILES = "validation/v2/sim_files/"
+SIM_PERCS = "validation/v2/sim_percs.json"
 
 START_DATE = datetime.datetime(1990, 1, 1)
 END_DATE = datetime.datetime(2024, 9, 1)
+
+PARQ_FOLDER = "validation/v2/parqs/"
 
 
 def get_tickers():
@@ -23,6 +25,14 @@ def get_tickers():
 
 def download_all_tickers(end_date):
     tickers = get_tickers()
-    failed = yfscraper.v2.download_data(tickers, BASE, end_date)
+    failed = yfscraper.download_data(tickers, BASE, end_date)
     with open(FAILED, "w") as file:
         json.dump(failed, file)
+
+
+def build_parq():
+    smarketsim.build_parq(BASE, PARQ_FOLDER)
+
+
+# download_all_tickers(END_DATE)
+# build_parq()
